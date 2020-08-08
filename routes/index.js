2edc1a2840bb4a573c3b147e1e7917594a8b02a1
Ben Arceneaux <ben.k.arceneaux@saic.com>
2020-08-08 13:02:51 -0500

    "Added Packages"

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,16 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next){
-    res.render('index', {title: 'My Site', condition: false})
+    res.render('index', {title: 'My Site', condition: false});
+    next();
 });
 
-router.post('/searchResults', function(req, res, next){
+router.get('/about', function(req, res, next) {
+    res.render('about', {title: 'About', condition: false});
+    next();
+});
+
+router.post('/', function(req, res, next){
     console.log("Search Terms: ", req.body.searchterms);
 });
 
